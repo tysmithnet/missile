ecf5cee3328eba6f00c5c6263d54f8c8fbf3019b
Ty Smith <dts5004@gmail.com>
2017-08-20 20:28:52 -0700

    Added ability to recognize global key events

--- src/Missile/Missile.Client/MainWindow.xaml.cs
+++ src/Missile/Missile.Client/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,10 +10,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Gma.System.MouseKeyHook;
 
 namespace Missile.Client
 {
@@ -20,9 +24,36 @@ namespace Missile.Client
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private IKeyboardMouseEvents globalEvents;
+        private IKeyboardMouseEvents appEvents;
         public MainWindow()
         {
             InitializeComponent();
+            CenterWindowOnScreen();
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+
+            globalEvents = Hook.GlobalEvents();
+            appEvents = Hook.AppEvents();
+            globalEvents.KeyDown += (sender, args) =>
+            {
+                Console.WriteLine(args.KeyValue);
+            };
+
+            appEvents.KeyDown += (sender, args) =>
+            {
+                Console.WriteLine(args.KeyValue);
+            };
         }
-    }
+
+        private void CenterWindowOnScreen()
+        {
+            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double windowWidth = this.Width;
+            double windowHeight = this.Height;
+            this.Left = (screenWidth / 2) - (windowWidth / 2);
+            this.Top = (screenHeight / 2) - (windowHeight / 2);
+        }
+    }    
 }
