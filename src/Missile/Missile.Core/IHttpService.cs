b24e0024ba49c205c26c6da4a35b4e5f6a3c4219
Ty Smith <dts5004@gmail.com>
2017-08-24 22:09:04 -0700

    Added IHttpService and corresponding implementation This is obviously not the final interface, but I don't want to add methods I don't need just yet.

---
+++ src/Missile/Missile.Core/IHttpService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missile.Core
+{
+    public interface IHttpService
+    {
+        Task<string> GetStringAsync(string url);
+    }
+}
