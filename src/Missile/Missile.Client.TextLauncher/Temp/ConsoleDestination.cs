207fcdcfcbe49299e3213f3730f5cc33a55dd64f
Ty Smith <dts5004@gmail.com>
2017-08-27 23:12:04 -0700

    Added temporary implementations to test out the API

---
+++ src/Missile/Missile.Client.TextLauncher/Temp/ConsoleDestination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missile.Client.TextLauncher.Temp
+{
+    public class ConsoleDestination : IDestination<string>
+    {
+        public void Process(IObservable<string> source)
+        {
+            source.Subscribe(Console.WriteLine, exception => Console.Error.WriteLine(exception),
+                () => Console.WriteLine("Complete"));
+        }
+    }
+}
---
+++ src/Missile/Missile.Client.TextLauncher/Temp/GoogleProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missile.Client.TextLauncher.Temp
+{
+    public class GoogleProvider : IProvider<GoogleResult>
+    {
+        public IObservable<GoogleResult> Get(string[] args)
+        {
+            var results = Enumerable.Range(1, 3).Select(x => new GoogleResult
+            {
+                Title = $"{x}",
+                Url =  $"http://wwww.{x}.com"
+            });
+            return results.ToObservable();
+        }
+    }
+
+    public class GoogleResult
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class GoogleStringConverter : IFilter<GoogleResult, string>
+    {
+        public IObservable<string> Filter(IObservable<GoogleResult> source)
+        {
+            return source.Select(x => $"{x.Title}: {x.Url}");
+        }
+    }
+}
