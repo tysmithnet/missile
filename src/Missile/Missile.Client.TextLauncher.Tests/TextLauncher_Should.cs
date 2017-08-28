40ac61c2c79ad95e2703491c258f433fb36e54af
Ty Smith <dts5004@gmail.com>
2017-08-27 23:23:23 -0700

    Added Missile.Client.TextLauncher.Tests

---
+++ src/Missile/Missile.Client.TextLauncher.Tests/TextLauncher_Should.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Missile.Client.TextLauncher.Tests
+{
+    public class TextLauncher_Should
+    {
+        [Fact]
+        public void Fail()
+        {
+             Assert.True(false);
+        }
+    }
+}
