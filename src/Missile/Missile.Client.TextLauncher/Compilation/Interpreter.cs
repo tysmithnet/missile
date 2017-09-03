5c12099990e4f102121550f9e99793fa90f76e3b
tysmithnet <dts5004@gmail.com>
2017-09-02 23:56:41 -0700

    Added some stub classes for conversion

--- src/Missile/Missile.Client.TextLauncher/Compilation/Interpreter.cs
+++ src/Missile/Missile.Client.TextLauncher/Compilation/Interpreter.cs
@@ -6,7 +6,23 @@ namespace Missile.Client.TextLauncher.Compilation
     {
         public void Interpret(RootNode root)
         {
-            
+
         }
+
+    }
+
+    public interface IConverter
+    {
+        
+    }
+
+    public interface IConverter<in TSource, out TDest> : IConverter
+    {
+        TDest Convert(TSource source);
+    }
+
+    public interface IConverterRepository
+    {
+        IConverter Get(Type source, Type dest);
     }
 }
