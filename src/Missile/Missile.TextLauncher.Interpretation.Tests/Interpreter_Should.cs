6ae738eb43132eb367138895cce4dd8f48af859b
Ty Smith <dts5004@gmail.com>
2017-09-04 22:50:22 -0700

    Added first failing test to Missile.TextLauncher.Interpretation.Tests

--- src/Missile/Missile.TextLauncher.Interpretation.Tests/Interpreter_Should.cs
+++ src/Missile/Missile.TextLauncher.Interpretation.Tests/Interpreter_Should.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
 
 namespace Missile.TextLauncher.Interpretation.Tests
 {
     public class Interpreter_Should
     {
+        [Fact]
         public void Handle_Single_Provider_With_No_Args()
-        {
-            
+        {                                                   
+            Interpreter interpreter = new Interpreter();
+            Task task = interpreter.Interpret(null);
+            task.Exception.Should().BeNull();
         }
     }
 }
---
+++ src/Missile/Missile.TextLauncher.Interpretation/Interpreter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missile.TextLauncher.Interpretation
+{
+    public class ProviderNode
+    {
+        public string RequestedProvider { get; set; }
+        public string ArgString { get; set; }
+
+        public ProviderNode(string requestedProvider, string argString)
+        {
+            RequestedProvider = requestedProvider ?? throw new ArgumentNullException(nameof(requestedProvider));
+            ArgString = argString ?? throw new ArgumentNullException(nameof(argString));
+        }
+    }
+
+    public class RootNode
+    {
+        public ProviderNode ProviderNode { get; set; }
+    }
+
+    public interface IInterpreter
+    {
+        Task Interpret(RootNode rootNode);
+    }
+
+    public class Interpreter : IInterpreter
+    {
+        public Task Interpret(RootNode rootNode)
+        {
+            return Task.FromException(new Exception("crashed"));
+        }
+    }
+}
