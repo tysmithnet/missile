12d5e6047aabe1bac4c0c0adffcd731820e18dc1
Ty Smith <dts5004@gmail.com>
2017-09-04 20:42:40 -0700

    Added Missile.TextLauncher.Interpretation project

---
+++ src/Missile/Missile.TextLauncher.Interpretation/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missile.TextLauncher.Interpretation
+{
+    public class Class1
+    {
+    }
+}
