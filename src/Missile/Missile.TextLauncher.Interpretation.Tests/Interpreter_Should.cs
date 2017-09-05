5acbd3ab1657eab26b430655156b843abe0d4aa1
Ty Smith <dts5004@gmail.com>
2017-09-04 22:38:26 -0700

    Added testing frameworks to Missile.TextLauncher.Interpretation.Tests

---
+++ src/Missile/Missile.TextLauncher.Interpretation.Tests/Interpreter_Should.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missile.TextLauncher.Interpretation.Tests
+{
+    public class Interpreter_Should
+    {
+        public void Handle_Single_Provider_With_No_Args()
+        {
+            
+        }
+    }
+}
