5e8888de35cbfc6efa984136af083e00ddea4b92
Ty Smith <dts5004@gmail.com>
2017-09-06 00:40:30 -0700

    Added Interpreter test for multiple filters

--- src/Missile/Missile.TextLauncher.Interpretation.Tests/Interpreter_Should.cs
+++ src/Missile/Missile.TextLauncher.Interpretation.Tests/Interpreter_Should.cs
@@ -30,7 +30,7 @@ namespace Missile.TextLauncher.Interpretation.Tests
             };
             var task = interpreter.Interpret(rootNode);
             Action action = () => task.Wait();
-            action.ShouldNotThrow("most basic case should always pass");
+            action.ShouldNotThrow("single provider should be no problem");
         }
 
         [Fact]
@@ -71,7 +71,7 @@ namespace Missile.TextLauncher.Interpretation.Tests
             
             var task = interpreter.Interpret(rootNodeBuilder.Build());
             Action action = async () => await task;
-            action.ShouldNotThrow("most basic cases should always pass");
+            action.ShouldNotThrow("provider and destination combo should work");
             sb.Length.Should().BeGreaterThan(0);
         }
 
@@ -81,7 +81,7 @@ namespace Missile.TextLauncher.Interpretation.Tests
             var rootNodeBuilder = new RootNodeBuilder();
             rootNodeBuilder
                 .WithProvider("range")
-                .WithFilter("log")
+                .WithFilter("take")
                 .WithDestination("console");
             var interpreter = new Interpreter();
             interpreter.ProviderRepository = new ProviderRepository();
@@ -101,7 +101,7 @@ namespace Missile.TextLauncher.Interpretation.Tests
             {
                 new RegisteredFilter
                 {
-                    Name = "log",
+                    Name = "take",
                     FilterInstance = new TakeFilter(),
                     FilterMethodInfo = typeof(TakeFilter).GetMethod("Filter")
                 }
@@ -125,7 +125,68 @@ namespace Missile.TextLauncher.Interpretation.Tests
 
             var task = interpreter.Interpret(rootNodeBuilder.Build());
             Action action = async () => await task;
-            action.ShouldNotThrow("most basic cases should always pass");
+            action.ShouldNotThrow("provider, filter, destination combo should work");
+            sb.Length.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void Handle_Multiple_Filters()
+        {
+            var rootNodeBuilder = new RootNodeBuilder();
+            rootNodeBuilder
+                .WithProvider("range")
+                .WithFilter("take")
+                .WithFilter("distinct")
+                .WithDestination("console");
+            var interpreter = new Interpreter();
+            interpreter.ProviderRepository = new ProviderRepository();
+            interpreter.ProviderRepository.RegisteredProviders = new List<RegisteredProvider>
+            {
+                new RegisteredProvider
+                {
+                    Name = "range",
+                    DestinationType = typeof(int),
+                    ProviderInstance = new RangeProvider(),
+                    ProvideMethodInfo = typeof(RangeProvider).GetMethod("Provide")
+                }
+            };
+
+            interpreter.FilterRepository = new FilterRepository();
+            interpreter.FilterRepository.RegisteredFilters = new List<RegisteredFilter>()
+            {
+                new RegisteredFilter
+                {
+                    Name = "take",
+                    FilterInstance = new TakeFilter(),
+                    FilterMethodInfo = typeof(TakeFilter).GetMethod("Filter")
+                },
+                new RegisteredFilter
+                {
+                    Name = "distinct",
+                    FilterInstance = new DistinctFilter(),
+                    FilterMethodInfo = typeof(DistinctFilter).GetMethod("Filter")
+                }
+            };
+
+            interpreter.DestinationRepository = new DestinationRepository();
+            StringBuilder sb = new StringBuilder();
+            interpreter.DestinationRepository.RegisteredDestinations = new List<RegisteredDestination>()
+            {
+                new RegisteredDestination
+                {
+                    Name = "console",
+                    SourceType = typeof(object),
+                    DestinationInstance = new ConsoleDestination
+                    {
+                        WriteFunction = o => sb.AppendLine($"{o}")
+                    },
+                    ProcessAsyncMethodInfo = typeof(ConsoleDestination).GetMethod("ProcessAsync")
+                }
+            };
+
+            var task = interpreter.Interpret(rootNodeBuilder.Build());
+            Action action = async () => await task;
+            action.ShouldNotThrow("interpreter should handle multiple filters");
             sb.Length.Should().BeGreaterThan(0);
         }
     }
