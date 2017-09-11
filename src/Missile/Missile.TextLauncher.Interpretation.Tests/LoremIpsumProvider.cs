f8d9e1d8185a5be0dcf74ffc8a84e520df52415a
Ty Smith <dts5004@gmail.com>
2017-09-10 17:53:40 -0700

    Added lorem ipsum provider

---
+++ src/Missile/Missile.TextLauncher.Interpretation.Tests/LoremIpsumProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missile.TextLauncher.Interpretation.Tests
+{
+    public class LoremIpsumProvider : IProvider<string>
+    {
+        public string Name { get; set; }
+        public IObservable<string> Provide()
+        {
+            return "Lorem ipsum dolor sit amet.".Split().ToObservable();
+        }
+    }
+}
