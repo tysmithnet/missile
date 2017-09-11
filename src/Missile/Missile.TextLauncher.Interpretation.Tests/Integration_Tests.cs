a1a21cae3fc9525ecdced958e966ed84e3e09b87
Ty Smith <dts5004@gmail.com>
2017-09-10 17:53:59 -0700

    Refactored integration tests

--- src/Missile/Missile.TextLauncher.Interpretation.Tests/Integration_Tests.cs
+++ src/Missile/Missile.TextLauncher.Interpretation.Tests/Integration_Tests.cs
@@ -7,8 +7,7 @@ namespace Missile.TextLauncher.Interpretation.Tests
 {
     public class Integration_Tests
     {
-        [Fact]
-        public void Handle_List_Output()
+        public static InterpretationFacade GetFacade()
         {
             var aggregateCatalog = new AggregateCatalog();
             var facadeAssembly = new AssemblyCatalog(typeof(InterpretationFacade).Assembly);
@@ -18,24 +17,30 @@ namespace Missile.TextLauncher.Interpretation.Tests
             var compositionContainer = new CompositionContainer(aggregateCatalog);
             var facade = new InterpretationFacade();
             compositionContainer.ComposeParts(facade);
+            return facade;
+        }
+
+        [Fact]
+        public void Handle_List_Output()
+        {   
             var input = "noop > list";
-            facade.Invoking(f => f.ExecuteAsync(input))
+            GetFacade().Invoking(f => f.ExecuteAsync(input))
+                .ShouldNotThrow("this is the most basic integration test possible");
+        }
+
+        [Fact]
+        public void Handle_Provider_Filter_Destination()
+        {   
+            var input = "noop | filter > noop";
+            GetFacade().Invoking(f => f.ExecuteAsync(input))
                 .ShouldNotThrow("this is the most basic integration test possible");
         }
 
         [Fact]
         public void Handle_Noop_Provider()
-        {
-            var aggregateCatalog = new AggregateCatalog();
-            var facadeAssembly = new AssemblyCatalog(typeof(InterpretationFacade).Assembly);
-            var providerAssembly = new AssemblyCatalog(typeof(IProvider<>).Assembly);
-            aggregateCatalog.Catalogs.Add(facadeAssembly);
-            aggregateCatalog.Catalogs.Add(providerAssembly);
-            var compositionContainer = new CompositionContainer(aggregateCatalog);
-            var facade = new InterpretationFacade();
-            compositionContainer.ComposeParts(facade);
+        {                                             
             var input = "noop";
-            facade.Invoking(f => f.ExecuteAsync(input))
+            GetFacade().Invoking(f => f.ExecuteAsync(input))
                 .ShouldNotThrow("this is the most basic integration test possible");
         }
     }
