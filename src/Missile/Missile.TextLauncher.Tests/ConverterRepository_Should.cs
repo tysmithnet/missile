a66ab480339bbebb4604a47eecb47e440143b695
Ty Smith <dts5004@gmail.com>
2017-09-13 22:13:32 -0700

    Added animal based hierarchy for converter tests

--- src/Missile/Missile.TextLauncher.Tests/ConverterRepository_Should.cs
+++ src/Missile/Missile.TextLauncher.Tests/ConverterRepository_Should.cs
@@ -11,42 +11,136 @@ namespace Missile.TextLauncher.Tests
 {
     public class ConverterRepository_Should
     {
-        public class ObjectStringConverter : IConverter<object, string>
+        private interface IHasLegs
         {
-            public IObservable<string> Convert(IObservable<object> source)
+
+        }
+
+        private interface IQuadraped : IHasLegs
+        {
+
+        }
+
+        private interface IBiPed : IHasLegs
+        {
+
+        }
+
+        private interface INoLegs
+        {
+
+        }
+
+        private class Mammal
+        {
+        }
+
+        private class Amphibion
+        {
+        }
+
+        private class Dog : Mammal, IQuadraped
+        {
+        }
+
+        private class Primate : Mammal, IBiPed
+        {
+        }
+
+        private class Corgi : Dog
+        {
+        }
+
+        private class Chimp : Primate
+        {
+        }
+
+        private class Turtle : Amphibion, IQuadraped
+        {
+        }
+
+        private class Leatherback : Turtle
+        {
+        }
+
+        private class Snake : Amphibion, INoLegs
+        {
+        }
+
+        private class Python : Snake
+        {
+        }
+
+        private class CorgiSnake : IConverter<Corgi, Snake>
+        {
+            public IObservable<Snake> Convert(IObservable<Corgi> source)
             {
-                return source.Select(o => o.ToString());
+                throw new NotImplementedException();
             }
         }
-        
+
+        private class CorgiPython : IConverter<Corgi, Python>
+        {
+            public IObservable<Python> Convert(IObservable<Corgi> source)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class DogSnake : IConverter<Dog, Snake>
+        {
+            public IObservable<Snake> Convert(IObservable<Dog> source)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class LegsToNoLegs : IConverter<IHasLegs, INoLegs>
+        {
+            public IObservable<INoLegs> Convert(IObservable<IHasLegs> source)
+            {
+                throw new NotImplementedException();
+            }
+        }
 
         [Fact]
         public void Return_Exact_Match_If_One_Exists()
         {
             ConverterRepository converterRepository = new ConverterRepositoryBuilder()
-                .WithConverter(new ObjectStringConverter())
+                .WithConverter(new CorgiSnake())
                 .Build();
 
-            var converter = converterRepository.Get(typeof(object), typeof(string));
-            converter.ConverterInstance.GetType().Should().Be(typeof(ObjectStringConverter), "exact matches should always return");
+            var converter = converterRepository.Get(typeof(Corgi), typeof(Snake));
+            converter.ConverterInstance.GetType().Should().Be(typeof(CorgiSnake), "exact matches should always return");
         }
 
         [Fact]
         public void Return_Compatible_Match_If_One_Exists()
         {
             ConverterRepository converterRepository = new ConverterRepositoryBuilder()
-                .WithConverter(new ObjectStringConverter())
+                .WithConverter(new DogSnake())
                 .Build();
 
-            var converter = converterRepository.Get(typeof(string), typeof(string));
-            converter.ConverterInstance.GetType().Should().Be(typeof(ObjectStringConverter), "compatible matches should return if the actual type isn't there");
+            var converter = converterRepository.Get(typeof(Corgi), typeof(Snake));
+            converter.ConverterInstance.GetType().Should().Be(typeof(DogSnake), "compatible matches should return if the actual type isn't there");
+        }
+
+        [Fact]
+        public void Return_Closest_Match()
+        {
+            //ConverterRepository converterRepository = new ConverterRepositoryBuilder()
+            //    .WithConverter(new )
+            //    .Build();
+
+            //var converter = converterRepository.Get(typeof(string), typeof(string));
+            //converter.ConverterInstance.GetType().Should().Be(typeof(ObjectStringConverter), "compatible matches should return if the actual type isn't there");
         }
 
         [Fact]
         public void Throw_If_No_Match()
         {
             ConverterRepository converterRepository = new ConverterRepositoryBuilder()
-                .WithConverter(new ObjectStringConverter())
+                .WithConverter(new CorgiPython())
                 .Build();
 
             converterRepository.Invoking(r => r.Get(typeof(string), typeof(int)))
