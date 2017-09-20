c1e2cbe926b74e316af45156d12a5eec7a40aed1
Ty Smith <dts5004@gmail.com>
2017-09-19 23:40:09 -0700

    Added outline for caching

---
+++ src/Missile/Missile.Core/ICache.cs
@@ -0,0 +1,13 @@
+﻿namespace Missile.Core
+{
+    public interface ICache<TKey, TResult>
+    {
+        TResult Get(TKey key);
+        void Set(TKey key, TResult value);
+    }
+
+    public class MemoryCache
+    {
+        
+    }
+}
