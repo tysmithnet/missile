1cf407280714cb89fd667e9a2118b6fb38904ad0
Ty Smith <dts5004@gmail.com>
2017-09-22 22:33:14 -0700

    Added GridDestination project stub

---
+++ src/Missile/Missile.TextLauncher.GridPlugin/GridDestination.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Missile.TextLauncher.Destination;
+using Missile.TextLauncher.Provision;
+
+namespace Missile.TextLauncher.GridPlugin
+{
+    [Export(typeof(IDestination))]
+    public class GridDestination : IDestination<GridDestinationItem>
+    {
+        
+        public string Name { get; set; } = "grid";
+        public Task ProcessAsync(IObservable<GridDestinationItem> source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GridDestinationItem : UserControl
+    {
+        
+    }
+}
