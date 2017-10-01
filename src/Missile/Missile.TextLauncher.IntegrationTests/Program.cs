63aaaa637c205c77b4ccda0aacda85a9931e5ce4
Ty Smith <dts5004@gmail.com>
2017-09-30 20:52:54 -0700

    Added Missile.TextLauncher.IntegrationTests (again) This time as a console application

---
+++ src/Missile/Missile.TextLauncher.IntegrationTests/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Missile.TextLauncher.IntegrationTests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new MissileClient())
+            {
+                Console.WriteLine("Loaded client successfully");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+
+    public sealed class MissileClient : IDisposable
+    {
+        private Process _process;
+
+        public MissileClient()
+        {
+            _process = Process.Start(Path.GetFullPath("../../../Missile.Client/bin/Debug/Missile.Client.exe"));
+        }
+
+        private void ReleaseUnmanagedResources()
+        {
+            _process.Kill();
+        }
+
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~MissileClient()
+        {
+            ReleaseUnmanagedResources();
+        }
+    }
+}
