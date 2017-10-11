ec71decb2886ef3173927b93dc1cc385cb172f9b
Ty Smith <dts5004@gmail.com>
2017-10-11 01:40:15 -0700

    Added docstrings

--- src/Missile/Missile.TextLauncher.ApplicationPlugin/ApplicationProvider.cs
+++ src/Missile/Missile.TextLauncher.ApplicationPlugin/ApplicationProvider.cs
@@ -7,16 +7,42 @@ using Missile.TextLauncher.Provision;
 
 namespace Missile.TextLauncher.ApplicationPlugin
 {
-    // todo: make it so you can right click certain types and get a context menu
+    /// <inheritdoc />
+    /// <summary>
+    ///     A provider that provides observable sequences of registered applications
+    /// </summary>
+    /// <seealso
+    ///     cref="!:Missile.TextLauncher.Provision.IProvider{Missile.TextLauncher.ApplicationPlugin.RegisteredApplication}" />
     [Export(typeof(IProvider))]
     [Export(typeof(ApplicationProvider))]
     public class ApplicationProvider : IProvider<RegisteredApplication>
     {
+        /// <summary>
+        ///     Gets or sets the application repository
+        /// </summary>
+        /// <value>
+        ///     The application repository
+        /// </value>
         [Import]
         protected internal IApplicationRepository ApplicationRepository { get; set; }
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Gets or sets the name of this provider
+        /// </summary>
+        /// <value>
+        ///     The name for this provider
+        /// </value>
         public string Name { get; set; } = "apps";
 
+        /// <inheritdoc />
+        /// <summary>
+        ///     Gets an observable sequence of registered applications based on the provided arguments
+        /// </summary>
+        /// <param name="args">Arguments for this provider</param>
+        /// <returns>
+        ///     An observable sequence of registered applications predicated on the provided arguments
+        /// </returns>
         public IObservable<RegisteredApplication> Provide(string[] args)
         {
             var options = new ApplicationProviderOptions();
