46a0281c5a4d14fffcd74d6d74fe8a486e87ac05
Ty Smith <dts5004@gmail.com>
2017-10-15 00:10:09 -0700

    Added stub tests for SettingsRepostiory

---
+++ src/Missile/Missile.TextLauncher.Tests/SettingsRepository_Should.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Xunit;
+
+namespace Missile.TextLauncher.Tests
+{
+    public class SettingsRepository_Should
+    {
+        private class FooSettings : ISettings, ISerializable
+        {
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class BarSettings : ISettings, ISerializable
+        {
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        [Fact]
+        public void Save_Serializable_Settings()
+        {
+            var settingsRepo =
+                new SettingsRepository {AllSettings = new ISettings[] {new FooSettings(), new BarSettings()}};
+            
+        }
+    }
+}
