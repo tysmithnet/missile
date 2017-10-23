9c72c5d6385b5f501d9b0d6458cf3373f734d758
Ty Smith <dts5004@gmail.com>
2017-10-22 20:01:14 -0700

    Moved settings to core

---
+++ src/Missile/Missile.Core.Implementation.Tests/BarSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Missile.TextLauncher.Tests
+{
+    [Serializable]
+    [ExcludeFromCodeCoverage]
+    public class BarSettings : ISettings
+    {
+        public int Y { get; set; }
+    }
+}
---
+++ src/Missile/Missile.Core.Implementation.Tests/FooSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Missile.TextLauncher.Tests
+{
+    [Serializable]
+    [ExcludeFromCodeCoverage]
+    public class FooSettings : ISettings
+    {
+        [Setting] public int Y;
+
+        [Setting]
+        public int X { get; set; }
+
+        [SubSettings]
+        public FooSubSettings FooSubSettings { get; set; } = new FooSubSettings();
+
+        public string SomethingElse { get; set; }
+    }
+
+    [Serializable]
+    [ExcludeFromCodeCoverage]
+    public class FooSubSettings : ISettings
+    {
+        public string X2 { get; set; }
+    }
+}
